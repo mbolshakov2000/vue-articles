e042ab3195f9713fd0e18af639096494dbae9c80
mbolshakov2000 <michaelbolshakov@me.com>
2022-04-29 19:28:29 +0300

    v8

---
+++ src/services/index.js
@@ -0,0 +1,37 @@
+import {Types} from '../store/types.js';
+import axios from 'axios'
+// import VueAxios from 'vue-axios'
+
+export default {
+    
+       fetchArticles(context){
+        if (this.cancel_source) {
+          this.cancel_source.cancel('Loading again!');
+          console.log('Request reloaded');
+        }
+        this.cancel_source = axios.CancelToken.source();
+  
+        context.commit(Types.mutations.ARTICLES_REQUESTED);
+        axios
+          .get('http://localhost:10000/articles',
+            {cancelToken: this.cancel_source.token}
+          )
+          .then(response => {
+            context.commit(Types.mutations.ARTICLES_SUCCEEDED, response.data);
+            console.log("Fetch data", response.data);
+          })
+          .catch(error => {
+            context.commit(Types.mutations.ARTICLES_FAILED, error);
+            console.log("Fetch error", error);
+          });
+      },
+      cancelFetchArticles(context){
+        if (this.cancel_source) {
+          this.cancel_source.cancel('Loading has been canceled!');
+          this.cancel_source = null;
+          console.log('Request canceled');
+        }
+        context.commit(Types.mutations.ARTICLES_FAILED, 'Loading has been canceled...');
+      }
+  
+}
